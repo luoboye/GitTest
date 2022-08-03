e57da0d454256e7c68468c0179d7df6796644c6e
luoboye <1216499350@github.com>
2022-08-03 11:15:06 +0800

    second commit

--- src/main/java/com/ye/GitTest.java
+++ src/main/java/com/ye/GitTest.java
@@ -3,5 +3,6 @@ package com.ye;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git 2.0");
     }
 }
